16f57ee07a1d9e40bdc84f3d7c320725f53e28f2
LE-S04 <win159x@yahoo.com.tw>
2020-04-22 18:52:45 +0800

    0422

--- lesson03/Case02.py
+++ lesson03/Case02.py
@@ -8,3 +8,9 @@ else:
     print('%d 奇數' % n)
 # 類三元運算子的寫法
 print('%d %s' % (n, "偶數" if n % 2 == 0 else "奇數"))
+
+
+# 建構是否是偶數的涵式 ?
+def isOdd(n):
+    return "偶數" if n % 2 == 0 else "奇數"
+print('%d %s' % (n, isOdd(n)))
