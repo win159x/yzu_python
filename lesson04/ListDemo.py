948a61401b978bdb1712030617ec79697d501240
LE-S05 <win159x@yahoo.com.tw>
2020-04-29 19:36:29 +0800

    0429

--- lesson04/ListDemo.py
+++ lesson04/ListDemo.py
@@ -2,3 +2,8 @@ import operator
 poker1 = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']
 poker2 = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']
 print(operator.eq(poker1, poker2))
+
+poker = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'] * 4
+print(poker)
+print(len(poker))
+print(poker.count('A'))
