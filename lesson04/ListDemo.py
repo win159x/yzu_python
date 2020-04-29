2c8443bf8932e18d2c39792484556d3ea46e5b37
LE-S05 <win159x@yahoo.com.tw>
2020-04-29 19:41:25 +0800

    0429

--- lesson04/ListDemo.py
+++ lesson04/ListDemo.py
@@ -1,4 +1,5 @@
 import operator
+import random as r
 poker1 = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']
 poker2 = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']
 print(operator.eq(poker1, poker2))
@@ -7,3 +8,10 @@ poker = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'] * 4
 print(poker)
 print(len(poker))
 print(poker.count('A'))
+# 抽一張牌
+card = poker.pop(0)
+print('抽第一張牌:', card)
+print(poker)
+# 洗牌
+r.shuffle(poker)
+print(poker)
