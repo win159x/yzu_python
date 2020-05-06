b4b01be63885a2dd5cd2e2fff70f85540952025e
LE-S04 <win159x@yahoo.com.tw>
2020-05-06 20:42:45 +0800

    0506

---
+++ lesson05/LambdaDemo2.py
@@ -0,0 +1,6 @@
+#利用Lambda製作出switch的運算邏輯
+sex = 1
+{1:lambda : print('男'),2:lambda : print('女')}.get(sex, lambda : print('錯誤'))()
+
+sex_info = {1:lambda x:print('男',x), 2:lambda x:print('女',x)}
+sex_info.get(1)(20)
