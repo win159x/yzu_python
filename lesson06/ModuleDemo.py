84023e09311e818a1b21e4ed0e0ba046f2043431
LE-S41 <win159x@yahoo.com.tw>
2020-05-13 19:42:36 +0800

    0513

--- lesson06/ModuleDemo.py
+++ lesson06/ModuleDemo.py
@@ -1,7 +1,11 @@
 import math
 import math as m
 from math import pi
+import lesson06.Utils as u
+from lesson06.Utils import sub
 
 print(math.pi)
 print(m.pi)
 print(pi)
+print(u.add(10, 20))
+print(sub(10, 20))
