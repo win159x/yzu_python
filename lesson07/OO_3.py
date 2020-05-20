3a969997e874cee4f064f4751381cc640b85f6c7
LE-S03 <win159x@yahoo.com.tw>
2020-05-20 19:43:08 +0800

    0520

--- lesson07/OO_3.py
+++ lesson07/OO_3.py
@@ -2,14 +2,20 @@ class Account:
     name = '' # 公有變數
     __balance = 100  # 私有變數
 
-    def addBalance(self, money):
+    def __init__(self, name, money):
+        self.name = name
         self.__balance = self.__balance + money
+
+    def addBalance(self, money):
+        if money > 0:
+            self.__balance = self.__balance + money
 
     def __str__(self):
         return self.name + "有 $" + str(self.__balance)
 
 if __name__ == '__main__':
-    acc = Account()
-    acc.name = 'Vincent'
-    acc.addBalance(200)
-    print(acc)
+    Account.bank = '元大銀行' # 靜態/類別變數
+    acc = Account('Vincent', 200)
+    print(Account.bank, acc)
+    acc2 = Account('Mary', 500)
+    print(Account.bank, acc2)
