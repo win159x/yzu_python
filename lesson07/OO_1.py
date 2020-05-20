07c19a2aff4942bb7df3044f17059521261b2372
LE-S03 <win159x@yahoo.com.tw>
2020-05-20 19:18:46 +0800

    0520

---
+++ lesson07/OO_1.py
@@ -0,0 +1,15 @@
+class Human:
+    name = ''
+    sex = ''
+    age = 0
+
+    def __str__(self): # self 相當於 java 中的 this
+        return self.name + "," + self.sex+"," + str(self.age)
+
+if __name__ == '__main__':
+    h = Human()
+    h.name = 'Vincent'
+    h.sex = '男'
+    h.age = 18
+    print(h.name,h.sex,h.age)
+    print(h)
