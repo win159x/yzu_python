156777d0a79d64cc838c094ae40807bf0283a2bd
LE-S41 <win159x@yahoo.com.tw>
2020-05-27 19:51:42 +0800

    0527

--- lesson08/gui/GUIDemo1.py
+++ lesson08/gui/GUIDemo1.py
@@ -4,7 +4,9 @@ win = tkinter.Tk()
 
 win.title('我的小視窗')
 label = tkinter.Label(win, text='Hello')
-label.pack()
+label.pack(side=tkinter.LEFT)
+label2 = tkinter.Label(win, text='Hello')
+label2.pack(side=tkinter.RIGHT)
 win.geometry('300x300')
 
 win.mainloop()
